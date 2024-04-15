f6b0b0c7562613185c42f7fd8c60eb9737e6d05d
Gabriel Martin <gabrielmartin13009@gmail.com>
2024-04-15 17:24:31 +0200

    first step

---
+++ script.js
@@ -0,0 +1,38 @@
+var map = L.map('map').setView([48.866667, 2.333333], 13);
+var div = document.getElementById('map');
+const popup = L.popup();
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([48.848870698431845, 2.388451479869548]).addTo(map);
+
+function markerClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Vous avez cliqué sur le marqueur aux coordonnées : " + e.latlng.toString() + " Mathilde est la goat")
+        .openOn(map);
+}
+
+marker.on('click', markerClick);
+
+var MARKERS_MAX = 4;
+
+// a layer group, used here like a container for markers
+var markersGroup = L.layerGroup();
+map.addLayer(markersGroup);
+
+map.on('click', function(e) {
+    // get the count of currently displayed markers
+    var markersCount = markersGroup.getLayers().length;
+
+    if (markersCount < MARKERS_MAX) {
+        L.marker(e.latlng).addTo(markersGroup);
+        return;
+    }
+
+    // remove the markers when MARKERS_MAX is reached
+    markersGroup.clearLayers();
+});
