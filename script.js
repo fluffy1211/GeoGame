3deeb2b085c8aaf87d7885b91283976f30c7c918
Gabriel Martin <gabrielmartin13009@gmail.com>
2024-04-17 15:51:48 +0200

    working

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
-var map = L.map('map').setView([48.866667, 2.333333], 13);
-const div = document.getElementById('map');
+// import "./node_modules/animate.css";
+
+var map = L.map('map').setView([48.866667, 2.333333], 2);
+var div = document.getElementById('map');
 const popup = L.popup();
-const btn = document.querySelector('.cityRequest');
-
-console.log(btn);
-
-// Get a random city
-
-btn.addEventListener('click', () => {
-    fetch('https://api.api-ninjas.com/v1/city?limit=1' , {
-        method: 'GET',
-        headers: {'X-Api-Key': 'I9rDpD1fxkDYiluZ6TQTyg==KdZHlXIUVj2pSjLj' },
-        contentType: 'application/json',
-    })
-        .then(response => response.json())
-        .then(data => {
-            const cities = data;
-            const randomIndex = Math.floor(Math.random() * cities.length);
-            const randomCity = cities[randomIndex];
-            const lat = randomCity.latitude;
-            const lon = randomCity.longitude;
-
-            // Display city name and coordinates on the page
-            const cityInfo = `City: ${randomCity.name}<br>Latitude: ${lat}<br>Longitude: ${lon}`;
-            const resultDiv = document.getElementById('question');
-            resultDiv.innerHTML = cityInfo;
-
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-
-
+var circle = L.circle();
+const btn = document.querySelector('.next');
+const button = document.querySelector('.button');
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -41,38 +13,110 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([48.848870698431845, 2.388451479869548]).addTo(map);
+L.tileLayer.provider('Esri.WorldImagery').addTo(map);
 
-function markerClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("Vous avez cliqué sur le marqueur aux coordonnées : " + e.latlng.toString() + " Mathilde est la goat")
-        .openOn(map);
-}
 
-marker.on('click', markerClick);
+// var marker = L.marker([48.848870698431845, 2.388451479869548]).addTo(map);
 
-var MARKERS_MAX = 4;
-
-// a layer group, used here like a container for markers
-var markersGroup = L.layerGroup();
-map.addLayer(markersGroup);
+var marker;
 
 map.on('click', function(e) {
-    // get the count of currently displayed markers
-    var markersCount = markersGroup.getLayers().length;
-
-    if (markersCount < MARKERS_MAX) {
-        var marker = L.marker(e.latlng).addTo(markersGroup);
-        marker.on('click', function(e) {
-            popup
-                .setLatLng(e.latlng)
-                .setContent("Vous avez cliqué sur le marqueur aux coordonnées : " + e.latlng.toString())
-                .openOn(map);
-        });
-        return;
+    if (marker) {
+        map.removeLayer(marker);
     }
-
-    // remove the markers when MARKERS_MAX is reached
-    markersGroup.clearLayers();
+    marker = L.marker(e.latlng).addTo(map);
+    marker.on('click', function(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("Vous avez cliqué sur le marqueur aux coordonnées : " + e.latlng.toString())
+            .openOn(map);
+    });
 });
+
+axios.get('https://restcountries.com/v3.1/all')
+    .then((response) => {
+        const countries = response.data;
+        const randomIndex = Math.floor(Math.random() * countries.length);
+        const randomCountry = countries[randomIndex];
+        const countryName = randomCountry.name.common;
+        const capital = randomCountry.capital[0];
+        const latlng = randomCountry.latlng;
+
+        const countryInfo = document.getElementById('countryinfo');
+        countryInfo.innerHTML = `
+        <div class="cards">
+        <h2>${countryName}</h2>
+        <h2>${capital}</h2>
+        <h2>${latlng}</h2>
+        </div>
+        `;
+
+
+        // Append the country info to a container outside the map
+        const container = document.getElementById('container');
+        container.appendChild(countryInfo);
+
+        // Set radius circle for the capital
+        const circle = L.circle(latlng, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 10000// Set the desired radius in meters
+        });  
+
+
+        var redmarker = L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        map.on('click', function(e) {
+            
+            if (!circle.getBounds().contains(e.latlng)) {
+            marker = L.marker(latlng).addTo(map);
+            redmarker = L.marker(latlng, {icon: redmarker}).addTo(map);
+            setTimeout(function() {                
+            alert("Tu pues la merde !");
+            }, 500);
+            } else {
+            marker = L.marker(latlng).addTo(map);               
+            redmarker = L.marker(latlng, {icon: redmarker}).addTo(map);
+            setTimeout(function() {  
+            alert("Bien joué...");
+            }, 500);
+            }
+
+            
+            const nextBtn = document.createElement('button');
+            nextBtn.className = 'animate__animated animate__bounceIn';
+            nextBtn.textContent = 'Next';
+            button.appendChild(nextBtn);
+
+            map.off('click');
+
+
+            nextBtn.addEventListener('click', function() {
+                location.reload();
+
+
+            });
+            
+        });
+
+
+        // Hide the circle by setting its style to transparent
+        circle.setStyle({ opacity: 0, fillOpacity: 0 });
+
+        // Add the circle to the map
+        map.addLayer(circle);
+            
+    })
+    .catch((error) => {
+        console.error(error);
+    }
+    );
+
